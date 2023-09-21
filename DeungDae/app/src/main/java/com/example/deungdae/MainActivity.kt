b27d346b0c0cb78359e2115b53d9e55434080f46
SeongWooHong <pluto0303@naver.com>
2023-09-21 19:29:59 +0900

    Version fix

--- DeungDae/app/src/main/java/com/example/deungdae/MainActivity.kt
+++ DeungDae/app/src/main/java/com/example/deungdae/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.deungdae
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen: SplashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
